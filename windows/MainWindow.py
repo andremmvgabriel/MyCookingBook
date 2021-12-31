b6bf39d99e10587beaa0913a551546f8110779fc
gabekebab <andremmvgabriel@gmail.com>
2021-12-31 01:24:54 +0000

    Improved Main Window functionality.

--- windows/MainWindow.py
+++ windows/MainWindow.py
@@ -1,5 +1,50 @@
+from pathlib import Path
 from .Window import Window
-
+from Application import Application
 class MainWindow(Window):
+    __books_path: Path = Path("books")
+
     def __init__(self) -> None:
         super().__init__("windows/designs/MainWindow.ui")
+        self.__books_path.mkdir(exist_ok=True)
+    
+    # Overridable Functions
+    def setup(self):
+        # Title
+        self.labelTitle.setText("Os Meus Livros de Receitas")
+
+        # Buttons
+        self.buttonOpen.clicked.connect(self.open_book)
+        self.buttonOpen.setText("Abrir")
+        self.buttonCreate.clicked.connect(self.create_book)
+        self.buttonImport.clicked.connect(self.import_book)
+
+        # Dropbox
+        self.entryBook.currentIndexChanged.connect(self.book_selected)
+
+        from PyQt5 import QtWidgets
+        #QtWidgets.QLabel().setTe
+        self.refresh()
+    
+    def refresh(self):
+        # Dropbox
+        for book_path in self.__books_path.glob("*.json"):
+            book_name = book_path.parts[-1].split(".")[0]
+            self.entryBook.addItem(book_name)
+        
+        self.entryBook.setCurrentIndex(-1)
+    
+    # Callbacks
+    def open_book(self):
+        print("openning")
+    
+    def create_book(self):
+        print("creating")
+        #Application.Windows.open("create_book")
+    
+    def import_book(self):
+        pass
+
+    def book_selected(self):
+        pass
+
--- windows/Window.py
+++ windows/Window.py
@@ -4,3 +4,9 @@ class Window(QtWidgets.QWidget):
     def __init__(self, ui_file: str) -> None:
         super(Window, self).__init__()
         uic.loadUi(ui_file, self)
+        self.setup()
+    
+    # Overridable functions
+    def setup(self): pass
+    def refresh(self): pass
+        
